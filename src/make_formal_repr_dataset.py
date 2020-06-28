80026e40c1075d309597c81ee4c3d0ec3d6d59d9
yummydum <sumita@nowcast.co.jp>
2020-06-28 11:37:31 +0900

    finish preprocess

--- src/make_formal_repr_dataset.py
+++ src/make_formal_repr_dataset.py
@@ -3,24 +3,40 @@ import re
 
 data_path = Path('../data/formal')
 
-for source in data_path.iterdir():
-    result_path = Path(str(source).replace('formal', 'formal_cleaned'))
-    with source.open(mode='r') as f_r:
-        with result_path.open(mode='w') as f_w:
-            for line in f_r:
 
-                if line.startswith('ID=') or line == 'FAILED!\n':
-                    continue
+def main():
 
-                # add space for tokenization
-                line = line.replace('(', ' ( ').replace(')', ' ) ')
+    token2id = dict()
 
-                # normalize variable num
-                for c in ['x', 'e']:
-                    variables = re.findall(f'{c}' + r'\d+', line)
-                    variables = sorted(list(set(variables)))
-                    for i, v in enumerate(variables):
-                        line = line.replace(v, f' {c}{i} ')
+    for source in data_path.iterdir():
+        tokenized_path = Path(str(source).replace('formal', 'formal_cleaned'))
+        encoded_path = Path(str(source).replace('formal', 'formal_encoded'))
+        with source.open(mode='r') as f_r:
+            with tokenized_path.open(mode='w') as f_w:
+                with encoded_path.open(mode='w') as f_w2:
+                    for line in f_r:
 
-                line = ' '.join(line.split())
-                f_w.write(line + '\n')
+                        if line.startswith('ID=') or line == 'FAILED!\n':
+                            continue
+
+                        # add space for tokenization
+                        line = line.replace('(', ' ( ').replace(')', ' ) ')
+
+                        # normalize variable num
+                        for c in ['x', 'e']:
+                            variables = re.findall(f'{c}' + r'\d+', line)
+                            variables = sorted(list(set(variables)))
+                            for i, v in enumerate(variables):
+                                line = line.replace(v, f' {c}{i} ')
+
+                        # Tokenized
+                        tokens = line.split()
+                        f_w.write(' '.join(tokens) + '\n')
+
+                        # Encoded
+                        encoded = []
+                        for t in tokens:
+                            if t not in token2id:
+                                token2id[t] = len(token2id.keys())
+                            encoded.append(token2id[t])
+                        f_w2.write(' '.join(encoded) + '\n')
